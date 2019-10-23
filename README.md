3ad2dc6c85aea3dfeeead73291e86358b28a3f66
Christophe BONNEFOY <christophe48bonnefoy@gmail.com>
2019-10-23 09:55:59 +0200

    Premier commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Ceci est un programme qui consiste à 
---
+++ spec/app_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/app.rb'
+
+describe 'perform method, it should perform' do
+  It' 'do
+  expect(perform().to eq '')
+  end
+end
